cf61756820fef96e4b6db36f40e8c2d4b97d60f5
merndevmohammad-design <merndevmohammad@gmail.com>
2026-04-01 10:15:33 +0500

    Initialize Node.js/Express project, environment config, folder structure

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require("express");
+require('dotenv').config();
 const app = express();
+const connectDb = require('./utils/db');
 
 app.get("/", (req, res) => {
   res.status(200).send("starting the new task managment project ");
 });
 
-const PORT = 5000;
-app.listen(PORT, () => {
-  console.log(`server is running at port: ${PORT}`);
+connectDb().then(() => {
+  const PORT = 5000;
+  app.listen(PORT, () => {
+    console.log(`Server is running at port: ${PORT}`);
+  });
 });
